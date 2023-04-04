d53fccb953e20a0aeeec4a0d4bcb91c5188f1cab
Sean Showalter <sean@showalter.design>
2023-04-04 17:51:32 +0000

    Tests passed.

--- src/main/java/Palindrome.java
+++ src/main/java/Palindrome.java
@@ -6,7 +6,7 @@ public class Palindrome {
      * You can solve this problem in several ways:
      * You could start with a String reversal algorithm, which you have already written, and check if str is the same
      * as its reversed form, or, you could check if the character at the nth position of a String moving forwards
-     * re the same as the nth position of a String moving backwards.
+     * re the same as the nth position of a String moving backwards. 
      *
      * @param str A String.
      * @return true if str is a palindrome, false otherwise.
